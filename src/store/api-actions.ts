8cf1f3701055064bbeadc54a2f6cd8e99ef6901d
Alexander Strakhovenko <sasstr@gmail.com>
2023-11-14 16:36:26 +0300

    feat: add actons for check authorizationStatus

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -1,10 +1,13 @@
 import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { APIRoute } from '../const';
+import { APIRoute, AuthorizationStatus } from '../const';
 import { AppDispatch } from '../types/state';
 import { OfferApi } from '../types/offer';
 import { State } from '../types/state';
 import { offersSlice } from './slices/offers';
+import { userSlice } from './slices/user';
+import { saveToken, dropToken } from '../services/token';
+import { AuthData, UserData } from '../types/user';
 
 export const fetchOffersAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -16,5 +19,47 @@ export const fetchOffersAction = createAsyncThunk<void, undefined, {
     const {data} = await api.get<OfferApi[]>(APIRoute.Offers);
 
     dispatch(offersSlice.actions.getOffers(data));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(userSlice.actions.setAuthorizationStatus(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(userSlice.actions.setAuthorizationStatus(AuthorizationStatus.NoAuth));
+    }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({login: email, password}, {dispatch, extra: api}) => {
+    const {data: {token}} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(token);
+    dispatch(userSlice.actions.setAuthorizationStatus(AuthorizationStatus.Auth));
+  },
+);
+
+export const logoutAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/logout',
+  async (_arg, {dispatch, extra: api}) => {
+    await api.delete(APIRoute.Logout);
+    dropToken();
+    dispatch(userSlice.actions.setAuthorizationStatus(AuthorizationStatus.NoAuth));
   },
 );
